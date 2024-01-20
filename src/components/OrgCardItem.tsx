5a4421c3ff0149272eb3e465a186780ed67e1e9a
widal001 <williamdaly422@gmail.com>
2024-01-20 15:53:14 -0500

    feat: Adds org dynamic route This is a dynamically generated page per organization.

--- src/components/OrgCardItem.tsx
+++ src/components/OrgCardItem.tsx
@@ -8,18 +8,27 @@ import {
 import BadgeList from "@/components/BadgeList";
 import type { OrgProps } from "@/types";
 
+const interactiveLinkClasses = `
+  hover:shadow-md hover:-translate-y-1
+  focus:shadow-md focus:-translate-y-1
+  `;
+
 export default function OrgCardItem(org: OrgProps) {
   return (
-    <Card className="min-w-72 flex flex-col overflow-hidden">
-      <CardHeader className="py-4 min-h-24 bg-slate-200">
-        <CardTitle className="text-xl tracking-normal">{org.name}</CardTitle>
-      </CardHeader>
-      <CardContent className="pb-4 pt-2 grow">
-        <p className="text-primary/70">{org.description}</p>
-      </CardContent>
-      <CardFooter>
-        <BadgeList badges={org.tags} />
-      </CardFooter>
-    </Card>
+    <a href={`/orgs/${org.id}`} className="flex">
+      <Card
+        className={`min-w-72 flex flex-col overflow-hidden ${interactiveLinkClasses}`}
+      >
+        <CardHeader className="py-4 min-h-24 bg-slate-200">
+          <CardTitle className="text-xl tracking-normal">{org.name}</CardTitle>
+        </CardHeader>
+        <CardContent className="pb-4 pt-2 grow">
+          <p className="text-primary/70">{org.description}</p>
+        </CardContent>
+        <CardFooter>
+          <BadgeList badges={org.tags} />
+        </CardFooter>
+      </Card>
+    </a>
   );
 }
