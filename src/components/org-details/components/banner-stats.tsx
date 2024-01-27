dab019450ae6b97c3300912275158bcf69b550e6
widal001 <williamdaly422@gmail.com>
2024-01-27 18:43:09 -0500

    fix: Warning about map without key in React component

--- src/components/org-details/components/banner-stats.tsx
+++ src/components/org-details/components/banner-stats.tsx
@@ -7,8 +7,8 @@ interface Props {
 export function BannerStats({ stats }: Props) {
   return (
     <div className="grid grid-cols-2 gap-4 text-center py-6">
-      {stats.map((stat) => (
-        <div className="flex flex-col justify-center pb-4">
+      {stats.map((stat, index) => (
+        <div className="flex flex-col justify-center pb-4" key={index}>
           <div className="text-3xl font-semibold text-foreground py-2">
             {stat.value}
           </div>
